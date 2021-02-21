8a4992c60b935d8c0adc7ff5216f29842d693a74
Yizhi Xu <mike007xu@gmail.com>
2021-02-20 19:40:36 -0500

    finish client send func

--- Week 08/client.js
+++ Week 08/client.js
@@ -22,11 +22,56 @@ class Request {
     this.headers["Content-Length"] = this.bodyText.length;
   }
 
-  send() {
+  send(connection) {
     return new Promise((resolve, reject) => {
-      //...
+      const parser = new ResponseParser();
+      if (connection) {
+        connection.write(this.toString());
+      } else {
+        connection = net.createConnection(
+          {
+            host: this.host,
+            port: this.port,
+          },
+          () => {
+            connection.write(this.toString());
+          }
+        );
+      }
+      connection.on("data", (data) => {
+        console.log(data.toString());
+        parser.receive(data.toString());
+        if (true) {
+          resolve(parser.response);
+          connection.end();
+        }
+      });
+      connection.on("error", (err) => {
+        reject(err);
+        connection.end();
+      });
     });
   }
+  toString() {
+    return (
+      `${this.method} ${this.path} HTTP/1.1\r\n` +
+      Object.keys(this.headers)
+        .map((key) => `${key}: ${this.headers[key]}`)
+        .join("\r\n") +
+      "\r\n\r\n" +
+      this.bodyText
+    );
+  }
+}
+
+class ResponseParser {
+  constructor() {}
+  receive(string) {
+    for (let i = 0; i < string.length; i++) {
+      this.receiveChar(string.charAt(i));
+    }
+  }
+  receiveChar(char) {}
 }
 void (async function () {
   let request = new Request({
